83b485fa7ac759c025e95d06ab788bc34781afba
djshs40-1401 <127316940+djshs40-1401@users.noreply.github.com>
2023-03-08 23:03:49 +0900

    UI 개선

--- 1.py
+++ 1.py
@@ -1,18 +1,21 @@
 import random
 import os
+import time
 
 class _word:
     word_num : int = 0
     word_eng : str = ""
-    word_meaning : str = ""
+    word_meaning : list[str] = []
 
     def __init__(self, str):
         self.word_num = int(str[0])
         self.word_eng = str[1]
-        self.word_meaning = str[2]
+        self.word_meaning = str[2:]
 
     def _print(self):
-        print(self.word_num,self.word_eng,self.word_meaning)
+        print(self.word_num,self.word_eng)
+        for i in word_meaning:
+            print(i,end=" ")
 
 
 
@@ -36,7 +39,7 @@ def word_list_print():
 init()
 
 
-_line = "---------------------------------------------------"
+_line = ""
 
 def start():
     n = int(input("개수를 입력하세요: "))
@@ -46,23 +49,32 @@ def start():
         b = random.randrange(0,2)
 
         if b == 0:
-            print("Q:",word_list[a].word_eng)
+            print("Q:",word_list[a].word_eng,_line)
             tr = input("뜻을 입력하세요: ")
-        
-            print("A:",word_list[a].word_meaning)
+
             
+            print("A:",end=" ")
+            for i in word_list[a].word_meaning:
+                print(i,end=" ")
             print(_line)
+            print(_line)
+            
         if b == 1:
-            print("Q:",word_list[a].word_meaning)
+            print("Q:",end=" ")
+            
+            for i in word_list[a].word_meaning:
+                print(i,end=" ")
+            print(_line)
+            
             tr = input("단어를 입력하세요: ")
-            print("A:",word_list[a].word_eng)
+            print("A:",word_list[a].word_eng,_line)
 
             print(_line)
 
 
 def end():
-    print('와 끝났어요')
-    os.system('pause')
+    print('와 끝났어요 영어바보에서 탈출하셨나요? (5초 뒤 종료)')
+    time.sleep(5)
 
 start()
 end()
